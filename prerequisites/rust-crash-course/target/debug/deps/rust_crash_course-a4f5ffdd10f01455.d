ab091ba1e1d06b8024401cb16d333836d4906d74
mankeylol <mankeyooooo@gmail.com>
2023-01-26 11:54:11 +0530

    Inital commit

---
+++ prerequisites/rust-crash-course/target/debug/deps/rust_crash_course-a4f5ffdd10f01455.d
@@ -0,0 +1,5 @@
+/Users/mayankjha/Mayank-Solana-Q1-2023/prerequisites/rust-crash-course/target/debug/deps/rust_crash_course-a4f5ffdd10f01455: src/main.rs
+
+/Users/mayankjha/Mayank-Solana-Q1-2023/prerequisites/rust-crash-course/target/debug/deps/rust_crash_course-a4f5ffdd10f01455.d: src/main.rs
+
+src/main.rs:
---
+++ prerequisites/rust-crash-course/target/debug/rust-crash-course.d
@@ -0,0 +1,1 @@
+/Users/mayankjha/Mayank-Solana-Q1-2023/prerequisites/rust-crash-course/target/debug/rust-crash-course: /Users/mayankjha/Mayank-Solana-Q1-2023/prerequisites/rust-crash-course/src/main.rs
